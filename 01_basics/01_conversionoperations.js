9cca63ba8fb74997a76fbb202eec967ff86b837a
anmolgoyal-dev <110355190+anmolgoyal-dev@users.noreply.github.com>
2024-03-14 09:54:59 +0000

    conersion of data types

---
+++ 01_basics/01_conversionoperations.js
@@ -0,0 +1,9 @@
+let score=33
+console.log(typeof score)
+console.log(typeof(score))
+// null is a typeof object.
+let a=11
+let b=String(a);
+console.log(b);
+console.log(typeof(b))
+//type of a is number and type of b is string as it is changed from number to string
